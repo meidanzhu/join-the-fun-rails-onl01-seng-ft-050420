c6776f8ee8311e0fc32d5776ff56d454d6f34bfd
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-07-14 23:11:01 +0000

    Automatically backed up by Learn

--- db/migrate/20200714230708_create_join_table_for_passenger_taxis.rb
+++ db/migrate/20200714230708_create_join_table_for_passenger_taxis.rb
@@ -1,4 +1,6 @@
-class CreateJoinTableForRides < ActiveRecord::Migration[5.0]
+class CreateJoinTableForPassengerTaxis < ActiveRecord::Migration[5.0]
   def change
+    create_join_table :passengers, :taxis do |t|
+
   end
 end
