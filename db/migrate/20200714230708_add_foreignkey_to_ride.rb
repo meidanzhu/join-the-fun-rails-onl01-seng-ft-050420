9e80fb16a662e3a1ae9aa313aa8ffe768cb823f7
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-07-14 23:16:46 +0000

    Automatically backed up by Learn

--- db/migrate/20200714230708_add_foreignkey_to_ride.rb
+++ db/migrate/20200714230708_add_foreignkey_to_ride.rb
@@ -1,8 +1,6 @@
-class CreateJoinTableForPassengerTaxis < ActiveRecord::Migration[5.0]
+class AddForeignKeyToRide < ActiveRecord::Migration[5.0]
   def change
-    create_join_table :passengers, :taxis do |t|
-      t.integer :passenger_id
-      t.integer :taxi_id
-    end
+    add_column :rides, :taxi_id, :integer
+    add_column :rides, :passenger_id, :integer
   end
 end
